64c8fd5b1da6ee05d79515bf48ac0a9a490494a9
Rasool Booley <45028873+RasoolBooley@users.noreply.github.com>
2020-12-22 17:17:33 +0200

    added skills & projects sections, finished retheme

--- frontend/static/js/views/Home.js
+++ frontend/static/js/views/Home.js
@@ -8,11 +8,7 @@ export default class extends AbstractView{
 
     async getHtml(){
         return `
-        <header class="main-header">
-            <h1>Rasool Booley</h1>
-            <p>Your Next Web Developer</p>
-            <a target="_blank" href="mailto:rasool.booley@gmail.com">Email Now!</a>
-        </header>
+        
         `;
     }
 }
